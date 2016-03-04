76b9c92b64f308e452087ab17bc358196a44ad15
Борис Фроликов < >
2016-03-04 21:12:19 +0300

    Заставил чат работать, передала его под модель контача

--- chatwindow.cpp
+++ chatwindow.cpp
@@ -4,32 +4,62 @@ chatWindow::chatWindow() : QWidget()
 {
     rec=NULL;
     tField=new QTextEdit;
+    tField->setReadOnly(1);
     sendButton=new QPushButton("send");
+    nameedit=new QLineEdit;
+    edit=new QLineEdit;
+    nameedit->setText("Ur name her");
     QVBoxLayout *layout=new QVBoxLayout;
+    layout->addWidget(nameedit);
     layout->addWidget(tField);
+    layout->addWidget(edit);
     layout->addWidget(sendButton);
     this->setLayout(layout);
     connect(sendButton,SIGNAL(clicked(bool)),this,SLOT(send()));
+    connect(sendButton,SIGNAL(clicked(bool)),this,SLOT(clearedit()));
 }
 
 chatWindow::chatWindow(chatWindow *c) : QWidget()
 {
     tField=new QTextEdit;
+    tField->setReadOnly(1);
     sendButton=new QPushButton("send");
     rec=c;
+    nameedit=new QLineEdit;
+    edit=new QLineEdit;
+    nameedit->setText("Ur name her");
     QVBoxLayout *layout=new QVBoxLayout;
+    layout->addWidget(nameedit);
     layout->addWidget(tField);
+    layout->addWidget(edit);
     layout->addWidget(sendButton);
     this->setLayout(layout);
     connect(sendButton,SIGNAL(clicked(bool)),this,SLOT(send()));
+    connect(sendButton,SIGNAL(clicked(bool)),this,SLOT(clearedit()));
 }
-void chatWindow::settext(QString st)
+
+void chatWindow::append(QString sta)
 {
-    tField->setText(st);
+    tField->append(sta);
+}
+
+void chatWindow::clear()
+{
+    tField->append(" ");
 }
 
 void chatWindow::send()
 {
-    rec->settext(tField->toPlainText());
+    rec->clear();
+    rec->append("["+nameedit->text()+"]:"+edit->text());
+    this->clear();
+    this->append("["+nameedit->text()+"]:"+edit->text());
+
+
+
+}
+void chatWindow::clearedit()
+{
+    edit->setText(" ");
 }
 
--- chatwindow.h
+++ chatwindow.h
@@ -4,22 +4,28 @@
 #include <QWidget>
 #include <QTextEdit>
 #include <QPushButton>
+#include <QLineEdit>
 class chatWindow : public QWidget
 {
     QTextEdit *tField;
     QPushButton *sendButton;
-
+    QLineEdit *nameedit;
+    QLineEdit *edit;
     Q_OBJECT
 public:
     chatWindow *rec;
     chatWindow();
     chatWindow(chatWindow *c);
-    void settext(QString);
+
+    void clear();
+    void append(QString);
+
 
 signals:
 
 public slots:
     void send();
+    void clearedit();
 };
 
 #endif // CHATWINDOW_H
